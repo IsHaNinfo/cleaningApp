6063951ae4f1f1e5c769721116560a6099f0d8da
ishaninfo <ishaninfo56@gmail.com>
2024-07-05 11:01:19 +0530

    add user part changed

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,7 +2,6 @@ import express from "express"
 
 const router = express.Router();
 import {registerAdmin,loginAdmin ,updateAdminPassword} from "../controllers/adminController.js"
-import adminAuthentication from "../middleware/adminAuthentication.js";
 router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
 router.put("/updateAdminPassword/:id", updateAdminPassword);
