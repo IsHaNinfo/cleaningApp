b6eb378d8c6f13318693065c0ce8f5c2380c9d1d
Geshan Pasindu <116612958+GeshanPasindu@users.noreply.github.com>
2024-07-10 20:44:31 +0530

    job signin signout ui

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -246,7 +246,7 @@ export const signInJob = async (req, res) => {
         job.assignedStaff = staffId;
         await job.save();
 
-        res.status(200).json({ response_code: 200, success: true, message: 'Staff signed in successfully', job });
+        res.status(200).json({ response_code: 200, success: true, message: 'Staff signed in successfully', data:job });
     } catch (error) {
         res.status(400).json({ response_code: 400, success: false, message: error.message });
     }
@@ -258,11 +258,12 @@ export const signOffJob = async (req, res) => {
 
     try {
         const job = await Job.findById(jobId);
+        console.log("job found",job);
         if (!job) {
             return res.status(404).json({ response_code: 404, success: false, message: 'Job not found' });
         }
 
-        if(!job.isSignOff == true){
+        if(job.isSignOff == true){
             return res.status(404).json({ response_code: 404, success: false, message: 'This Job is already sign off' });
         }
 
@@ -279,7 +280,7 @@ export const signOffJob = async (req, res) => {
 
         await job.save();
 
-        res.status(200).json({ response_code: 200, success: true, message: 'Staff signed off successfully', job });
+        res.status(200).json({ response_code: 200, success: true, message: 'Staff signed off successfully', data:job });
     } catch (error) {
         res.status(400).json({ response_code: 400, success: false, message: error.message });
     }
